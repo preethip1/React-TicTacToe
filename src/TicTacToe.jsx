8e62acec84816fbb0f3797738d9502e617aed9ee
Preethi <preethiknot1998@gmail.com>
2021-12-14 16:17:10 +0530

    tictactoe functionality

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,53 +1,153 @@
 import React from 'react';
 import Block from './Block';
-import { v4 as uuidv4 } from 'uuid';
-
-
 
 class TicTacToe extends React.Component {
-    id = uuidv4()
-    state = {
-        firstMove : "X",
-        blocks : []
+    state ={
+        blocks : [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        currrentPlayer : 'X',
+        winner : ''
     }
-    
-    clickAssignment(id) {
-        this.state.blocks.forEach((element,index) => {
-            console.log(element,index);
-            // if(element.id === id) {
-            //     console.log(element.id);
-            // }
+
+    clickAssignment(e,index){
+        if (this.state.winner !== ''){
+            return
+        }
+        if(this.state.blocks[index] === -1){
+            let target = e.target;
+            target.innerText = this.state.currrentPlayer;
+            let updatedBlocks = this.state.blocks;
+            updatedBlocks[index] = this.state.currrentPlayer
+            this.setState({blocks : updatedBlocks});
+            let result = this.WinningCondition();
+            if(result === -1) {
+                if(this.state.currrentPlayer === "X") {
+                    this.setState({currrentPlayer : 'O'});
+                } else {
+                    this.setState({currrentPlayer : 'X'})
+                }
+            } else {
+                this.setState({winner:result})
+                 setTimeout(() => {
+                    if(result === 'D') {
+                        alert("Draw");
+                    }
+                    if(result === 'X') {
+                        alert("Player 1 is the winner");
+                    }
+                    if(result === 'O') {
+                        alert("Player 2 is the winner");
+                    }
+                 }, 1); 
+            }
+        } 
+    } 
+
+    WinningCondition() {
+        let winner = -1
+        let blocks = this.state.blocks;
+        if(blocks[0] === blocks[1] && blocks[1] === blocks[2]) {
+            if(blocks[0] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[0] === 'O') {
+                winner = 'O'
+                return winner;
+            }
+        }
+         if(blocks[3] === blocks[4] && blocks[4] === blocks[5]) {
+            if(blocks[3] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[3] === 'O') {
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[6] === blocks[7] && blocks[7] === blocks[8]) {
+            if(blocks[6] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[6] === 'O') {
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[0] === blocks[3] && blocks[3] === blocks[6]) {
+            if(blocks[0] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[0] === 'O'){
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[1] === blocks[4] && blocks[4] === blocks[7]) {
+            if(blocks[4] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[4] === 'O'){
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[2] === blocks[5] && blocks[5] === blocks[8]) {
+            if(blocks[2] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[2] === 'O'){
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[0] === blocks[4] && blocks[4] === blocks[8]) {
+            if(blocks[4] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[4] === 'O') {
+                winner = 'O'
+                return winner;
+            }
+        }
+        if(blocks[2] === blocks[4] && blocks[4] === blocks[6]) {
+            if(blocks[4] === 'X') {
+                winner = 'X'
+                return winner;
+            } else if(blocks[4] === 'O') {
+                winner = 'O'
+                return winner;
+            }
+        }
+        let count = 0;
+        this.state.blocks.forEach((element) => {
+            if(element === -1) {
+               count = count + 1; 
+            }
         })
-        // console.log("clicked");
-        if(this.state.firstMove === "X") {
-            this.setState({firstMove: "O"})
-            console.log("move",this.state.firstMove)
-
+        if(count === 0) {
+            winner = 'D';
+            return winner
+        } else {
+            winner = -1 
+            return winner 
         }
-        else {
-            this.setState({firstMove : "X"})
-            console.log("move",this.state.firstMove)
-
-        }
-
     }
 
     render() { 
         return <div>
             <div className='flex flex-row'>
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,0)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,1)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,2)}><Block/></div> 
             </div>
             <div className='flex flex-row'>
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,3)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,4)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,5)}><Block/></div> 
             </div>
             <div className='flex flex-row'>
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
-                <div onClick={() => this.clickAssignment()}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,6)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,7)}><Block/></div> 
+                <div onClick={(e) => this.clickAssignment(e,8)}><Block/></div> 
             </div>
         </div>;
     }
